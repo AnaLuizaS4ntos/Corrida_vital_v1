01a3d36d82ad878837e466fbf0d7d96922b99967
Gabriella Martins <anabebela10@gmail.com>
2026-06-05 08:06:31 -0300

    Player indo de um lado pro outro

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,11 +1,18 @@
-extends Node2D
+extends CharacterBody2D
 
+# Velocidade do movimento do player
+@export var velocidade: float = 300.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	# Aqui trocamos para "left" e "right", exatamente como está na imagem image_a64127.png
+	var direcao = Input.get_axis("left", "right")
+	
+	if direcao:
+		# Se estiver pressionando algo, move o player no eixo X
+		velocity.x = direcao * velocidade
+	else:
+		# Se soltar as teclas, o player para suavemente
+		velocity.x = move_toward(velocity.x, 0, velocidade)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	# Aplica o movimento com física e colisão
+	move_and_slide()
