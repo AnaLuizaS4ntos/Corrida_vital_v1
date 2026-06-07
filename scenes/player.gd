60e0e61a191cd206651b5d8ef03b0655ab4bc0bf
Gabriella Martins <anabebela10@gmail.com>
2026-06-07 19:15:00 -0300

    colisao player faz itens sumirem

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,8 +4,8 @@ extends CharacterBody2D
 @export var velocidade: float = 300.0
 
 func _physics_process(delta: float) -> void:
-	# Aqui trocamos para "left" e "right", exatamente como está na imagem image_a64127.png
-	var direcao = Input.get_axis("ui_left", "ui_right")
+	
+	var direcao = Input.get_axis("left", "right")
 	
 	if direcao:
 		# Se estiver pressionando algo, move o player no eixo X
@@ -16,3 +16,18 @@ func _physics_process(delta: float) -> void:
 
 	# Aplica o movimento com física e colisão
 	move_and_slide()
+
+
+# --- FUNÇÃO QUE DETECTA OS ITENS DA PISTA ---
+func _on_detector_de_itens_area_entered(area: Area2D) -> void:
+	# Verificamos se o objeto em que o player encostou possui a variável 'eh_saudavel'
+	if "eh_saudavel" in area:
+		if area.eh_saudavel:
+			# Código quando toca em algo Saudável (Frutas)
+			print("Item Saudavel coletado! (Sumindo)")
+		else:
+			# Código quando toca em algo Não Saudável (Hambúrguer, cigarro, etc.)
+			print("Obstaculo Nao Saudavel atingido! (Sumindo)")
+		
+		# Esta linha deleta o item do jogo imediatamente ao toque
+		area.queue_free()
